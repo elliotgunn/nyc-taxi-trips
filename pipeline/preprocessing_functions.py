ef0f0c1810dd80f4e251f61f1e8d67f5ee4b3ce2
elliotgunn <elliot.j.gunn@gmail.com>
2020-04-09 23:51:56 -0400

    preprocessing func

--- pipeline/preprocessing_functions.py
+++ pipeline/preprocessing_functions.py
@@ -5,7 +5,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 # feature scaling
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 
 # to build the models
 from sklearn.linear_model import LinearRegression, Lasso
@@ -56,3 +56,31 @@ def to_str(train, test, var):
 ## add def reduce_cardinality
 
 ## add def categorical_encoders
+
+
+def train_scaler(df, output_path):
+    scaler = MinMaxScaler()()
+    scaler.fit(df)
+    joblib.save(scaler, output_path)
+    return scaler
+
+def scale_features(df, scaler):
+    scaler = load(scaler) # with joblib probably
+    return scaler.transform(df)
+
+
+## make this model agnostic
+def train_model(df, target, features, scaler, output_path):
+    lasso = Lasso(random_state=1)
+    lasso.fit(scaler.transform(df[features]), target)
+    joblib.save(lin_model, output_path)
+    return lin_model
+
+def predict(df, model, features, scaler):
+    return model.predict_proba(scaler.transform(df[features]))
+
+def train_feature_selector(df, output_path):
+    selector = sklearn_selector()
+    selector.fit(df)
+    joblib.save(selector, output_path)
+    return selector
