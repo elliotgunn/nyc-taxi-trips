1ef9ea949f2eaffb00c0f7381e5511cea0a566d8
elliotgunn <elliot.j.gunn@gmail.com>
2020-04-12 21:21:09 -0400

    added features to config

---
+++ pipeline/config.py
@@ -0,0 +1,16 @@
+PATH_TO_DATASET = "houseprice.csv"
+OUTPUT_SCALER_PATH = 'scaler.pkl'
+OUTPUT_MODEL_PATH = 'lasso_regression.pkl'
+
+# preprocessing
+TARGET = 'tip_amount'
+
+NEG_COLS = ['fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount', 'improvement_surcharge', 'total_amount']
+
+ZERO_COLS = ['fare_amount', 'total_amount']
+
+TIME_COLS = ['tpep_pickup_datetime', 'tpep_dropoff_datetime']
+
+LOG_VARS = ['fare_amount', 'total_amount']
+
+VAR_TO_STR = ['VendorID', 'RatecodeID', 'PULocationID', 'DOLocationID', 'store_and_fwd_flag', 'payment_type']
